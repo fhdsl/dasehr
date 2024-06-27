bbfe8ebf409c4669404009afd25d1635f2cad917
avahoffman <avamariehoffman@gmail.com>
2024-06-27 11:31:47 -0400

    try emo

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname){
-  message(paste("Loading", pkgname, "🌼"))
+  message(paste("Loading", pkgname, emo::ji("blossom")))
   message(paste("Run ?dasehr for more information on this package its datasets."))
 }
