0a9aa5c6b5452dff900ada71f7bced62c75d4f2b
avahoffman <avamariehoffman@gmail.com>
2024-06-27 11:22:04 -0400

    switch to onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname){
   message(paste("Loading", pkgname, "🌼"))
   message(paste("Run ?dasehr for more information on this package its datasets."))
 }
