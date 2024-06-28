3af146131718b43c8147fb9c83efba2917a18c2e
avahoffman <avamariehoffman@gmail.com>
2024-06-27 22:35:53 -0400

    Add covid_wastewater

--- R/data.R
+++ R/data.R
@@ -1,3 +1,6 @@
+# This file contains code to read in datasets for DaSEH automatically. Raw rda files are located in the "data" directory.
+
+
 #' CalEnviroScreen 4.0 Data
 #'
 #' A dataset used in the DaSEH Short Course. The CalEnviroScreen object contains data from CalEnviroScreen 4.0, including the environmental dataset and the demographic dataset.
@@ -78,3 +81,30 @@
 #' @examples
 #' data(calenviroscreen)
 "calenviroscreen"
+
+
+#' SARS-CoV-2 Wastewater Data
+#'
+#' A dataset used in the DaSEH Short Course. More here..
+#'
+#' @format A data frame with x rows and x columns.
+#' \describe{
+#' \item{reporting_jurisdiction}{}
+#' \item{sample_location}{}
+#' \item{key_plot_id}{}
+#' \item{county_names}{}
+#' \item{population_served}{}
+#' \item{date_start}{}
+#' \item{date_end}{}
+#' \item{rna_pct_change_15d}{}
+#' \item{pos_PCR_prop_15d}{}
+#' \item{percentile}{}
+#' \item{sampling_prior}{}
+#' \item{first_sample_date}{}
+#'}
+#'
+#' @source Created using data downloaded from x.
+#'
+#' @examples
+#' data(covid_wastewater)
+"covid_wastewater"
