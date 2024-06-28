a77ab4d42d22d13a2ba6c2f808e00ed7255606d4
avahoffman <avamariehoffman@gmail.com>
2024-06-27 23:37:13 -0400

    Update package info

--- R/dasehr-package.R
+++ R/dasehr-package.R
@@ -5,17 +5,29 @@
 #'
 #' Here is a list of the datasets. They are already stored as objects when you
 #' load the package.
+#'
 #' * `calenviroscreen`: Description here
+#'
 #' * `CO_heat_ER_byage`: Description here
+#'
 #' * `CO_heat_ER_bygender`: Description here
+#'
 #' * `CO_heat_ER_wide`: Description here
+#'
 #' * `CO_heat_ER`: Description here
+#'
 #' * `covid_wastewater`: Description here
+#'
 #' * `er_CO_statewide`: Description here
+#'
 #' * `er_visits_by_CO_county`: Description here
+#'
 #' * `haa5`: Description here
+#'
 #' * `nitrate`: Description here
+#'
 #' * `yearly_co2_emissions`: Description here
+#'
 
 #' @keywords internal
 "_PACKAGE"
